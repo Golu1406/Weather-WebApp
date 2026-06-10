7e8a1003e7f043f245d9337fa4dbbb83564f74ca
Golu Thakur <golut0160@gmail.com>
2026-06-10 16:20:30 +0530

    Replace API key and update API URL format

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const apiKey = "2cde870a23674fff956f5d34390af5f2";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiKey = "Paste Your own API";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search-btn");
